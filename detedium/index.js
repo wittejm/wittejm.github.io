4d184b34a8d822e9230236631953fbc425f611a1
Jordan Witte <wittejm@gmail.com>
2022-10-12 19:46:47 -0700

    clean 1-line inputs and MVPgit add detedium/

--- detedium/index.js
+++ detedium/index.js
@@ -1,18 +1,31 @@
-const prompts = [
+const data = [
   [
-    ['b', 'b','g','y','g'],
-    ['b', 'g','g','y','g'],
-    ['g', 'g','g','g','g']
+    480,
+    [
+      ['y', 'b','b','b','b'],
+      ['b', 'b','b','y','b'],
+      ['b', 'y','b','g','g'],
+      ['g', 'g','g','g','g'],
+    ],
+    ['clued', 'aback', 'mimic', 'ionic']
   ],
-];
-
-const allAnswers = [
-  ['hello', 'large', 'world']
-];
+  [
+    479,
+    [
+      ['b', 'y','b','b','b'],
+      ['b', 'g','g','b','b'],
+      ['y', 'g','g','b','b'],
+      ['g', 'g','g','g','g'],
+    ],
+    ['clots', 'ralph', 'dally', 'valid']
+  ],
+]
 
 let guesses = [];
-
-const numWords = prompts[0].length;
+let activeDay = 0;
+let numWords = data[activeDay][1].length;
+let answers = data[activeDay][2];
+let prompt = data[activeDay][1];
 
 function createSquare(color) {
   let li = document.createElement('div');
@@ -20,41 +33,71 @@ function createSquare(color) {
   return li;
 }
 
-function normalizeInput() {
-  let inputArea = document.getElementById('guessInput');
+function normalizeInput(inputArea) {
   const cursorLocation = inputArea.selectionStart;
-  inputArea.value =  inputArea.value.toUpperCase().replace(/[^a-zA-Z]/, "").slice(0,5 * numWords);
+  inputArea.value =  inputArea.value.toUpperCase().replace(/[^a-zA-Z]/, "").slice(0,5);
   inputArea.selectionStart = cursorLocation;
   inputArea.selectionEnd = cursorLocation;
   let submitButton = document.getElementById("submitButton");
-  console.log("norm");
-  console.log("inputArea.value.length", inputArea.value.length);
 
-  if (inputArea.value.length === 5 * numWords) {
+  if (inputFields.every(i=>i.value.length === 5)) {
     submitButton.disabled=false;
   } else submitButton.disabled=true;
 }
 
+function bygSingleWord(guess, truth) {
+  const greenMarked = new Array(5).fill(0);
+  const yellowMarked = new Array(5).fill(0);
+  let updatedTruth = truth;
+
+  guess.split("").map((_,i)=>{
+    guessLetter = guess[i];
+    truthLetter = updatedTruth[i];
+    if(guessLetter===truthLetter) {
+      greenMarked[i]=1;
+      updatedTruth= updatedTruth.slice(0,i)+"_"+(updatedTruth.slice(i+1));
+    } 
+  });
+
+  guess.split("").map((_,i)=>{
+    guessLetter = guess[i];
+    truthLetter = updatedTruth[i];
+    if (updatedTruth.includes(guessLetter)) {
+      yellowMarked[i]=1;
+      updatedTruth= updatedTruth.slice(0,updatedTruth.indexOf(guessLetter))+"_"+(updatedTruth.slice(updatedTruth.indexOf(guessLetter)+1));
+    }
+  });
+
+  return guess.split("").map((_,i)=>{
+    guessLetter = guess[i];
+    truthLetter = updatedTruth[i];
+    if(greenMarked[i]) return "g";
+    if(yellowMarked[i]) return "y";
+    return "b";
+  });
+}
+
 function submit() {
-  let inputArea = document.getElementById('guessInput');
+
   const guesses = new Array(numWords).fill(0).map(
-    (_,i)=>inputArea.value.slice(i * 5, (i+1) * 5).split(""));
+    (_,i)=>inputFields[i].value.split(""));
   console.log("guesses", guesses);
   const responsesDiv = document.getElementById("responsesDiv");
   let response = document.createElement('div');
+  response.className = 'response'
   responsesDiv.appendChild(response);
   guesses.map((guess, i)=>{
     console.log("guess", guess)
     let rowDiv = document.createElement('div');
     rowDiv.className = `r`;
     response.appendChild(rowDiv);
-    const answers = allAnswers[0];
+    const byg = bygSingleWord(guess.join(""), answers[i].toUpperCase());
     guess.map((letter, j)=>{
       let letterDiv = document.createElement('div');
       console.log("i j", i, j);
       console.log("letter", letter);
       console.log("answer[i][j]", answers[i][j].toUpperCase(), letter);
-      const color = letter===answers[i][j].toUpperCase() ? "g" : answers[i].toUpperCase().includes(letter)? "y" : "b"; 
+      const color = byg[j];
       letterDiv.className = `s l ${color}`;
       rowDiv.appendChild(letterDiv);
       letterDiv.append(letter)
