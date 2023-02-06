e1a442cc3fe4e30ca2a97408a8b17d61313d003a
Jordan Witte <wittejm@gmail.com>
2023-02-06 13:44:48 -0800

    4 new words

--- detedium/data.js
+++ detedium/data.js
@@ -1,5 +1,25 @@
 const data = [
   [
+    596,
+    [],
+    ['wrong','sneak','punty','linty','ninth']
+  ],
+  [
+    595,
+    [],
+    ['cargo','batch','fancy','dance']
+  ],
+  [
+    594,
+    [],
+    ['scour','whump','guide','unity','until','unlit']
+  ],
+  [
+    593,
+    [],
+    ['under','chill','smoky','gassy','pasty','tasty']
+  ],
+  [
     592,
     [],
     ['sicks','scalp','scold']
